0eca61ffc744868e34bf033e643b882a18120945
Parth Raghav <iniento12@gmail.com>
2020-09-19 16:09:51 +0530

    :wrench: Adding config

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+// class name of div blocks where sniffed data is stored
+const INTERCEPTED_RESPONSE_DOM_CLASSNAME = "__interceptedData";
+// timeout for the idle callbacks
+const SCRAPE_SNIFFER_RESPONSE_TIMEOUT = 10000;
+// flag to scrape only until all div blocks are exhausted
+const SCRAPE_UNTIL_EXHAUSTED = false;
+export {
+    INTERCEPTED_RESPONSE_DOM_CLASSNAME,
+    SCRAPE_SNIFFER_RESPONSE_TIMEOUT,
+    SCRAPE_UNTIL_EXHAUSTED,
+};
