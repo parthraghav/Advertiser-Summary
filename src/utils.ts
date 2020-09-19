9d1b0b81ee50588d3068ea5b277efd8bb13975f1
Parth Raghav <iniento12@gmail.com>
2020-09-19 17:30:34 +0530

    :beers: Adding utilities

---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+/**
+ * Get the URL parameters
+ * source: https://css-tricks.com/snippets/javascript/get-url-variables/
+ * @param  {String} url The URL
+ * @return {Object}     The URL parameters
+ */
+export const getParams = (url: string) => {
+    var params: any = {};
+    var parser = document.createElement("a");
+    parser.href = url;
+    var query = parser.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        params[pair[0]] = decodeURIComponent(pair[1]);
+    }
+    return params;
+};
+
+export const getParam = (url: string, param: string) => {
+    let param_key_value_pair = getParams(url);
+    return param_key_value_pair[param];
+};
