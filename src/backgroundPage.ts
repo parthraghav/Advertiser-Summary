4ee4bbe0cdecbab2e2a9a630eab99a5e3434bfb4
Parth Raghav <iniento12@gmail.com>
2020-09-19 23:32:00 +0530

    :alien: Enabling message relaying between scripts

--- src/backgroundPage.ts
+++ src/backgroundPage.ts
@@ -1,10 +1,17 @@
 import { browser } from "webextension-polyfill-ts";
+import * as config from "@src/config";
 
 // Listen for messages sent from other parts of the extension
-browser.runtime.onMessage.addListener((request: { popupMounted: boolean }) => {
-    // Log statement if request.popupMounted is true
-    // NOTE: this request is sent in `popup/component.tsx`
-    if (request.popupMounted) {
-        console.log("backgroundPage notified that Popup.tsx has mounted.");
-    }
+browser.runtime.onConnect.addListener(port => {
+    console.assert(port.name == config.PORT_NAME);
+    port.onMessage.addListener(function(msg) {
+        if (msg.subject == "NEW_AD_DATA" && msg.from == "Content") {
+        }
+        // if (msg.joke == "Knock knock")
+        //     port.postMessage({ question: "Who's there?" });
+        // else if (msg.answer == "Madame")
+        //     port.postMessage({ question: "Madame who?" });
+        // else if (msg.answer == "Madame... Bovary")
+        //     port.postMessage({ question: "I don't get it." });
+    });
 });
