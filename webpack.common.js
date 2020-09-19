899012ddc6584d0c24a4854b767a07979a9aeb04
Parth Raghav <iniento12@gmail.com>
2020-09-19 14:37:50 +0530

    :construction_worker: Adding an entry point

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
     entry: {
+        contentScript: path.join(__dirname, "src/contentScript.ts"),
         backgroundPage: path.join(__dirname, "src/backgroundPage.ts"),
         popup: path.join(__dirname, "src/popup/index.tsx"),
     },
