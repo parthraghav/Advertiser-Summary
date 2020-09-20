2cfae631ee4318b2670c23f2e68a662a3cec995c
Parth Raghav <iniento12@gmail.com>
2020-09-20 10:26:05 +0530

    :art: Altered table

--- src/database.ts
+++ src/database.ts
@@ -20,27 +20,53 @@ export default class Database {
             "platform",
             "url",
             "last_visited",
+            "view_count",
         ]);
         this.db.createTable("advertiser_views", [
             "uuid",
             "advertiser_uuid",
+            "advertiser_name",
             "timestamp",
             "platform",
         ]);
         this.db.commit();
     }
+    get_all_advertiser_data() {
+        return this.db.queryAll("advertiser_data", {
+            sort: [
+                ["view_count", "DESC"],
+                ["last_visited", "DESC"],
+            ],
+        });
+    }
+    get_all_advertiser_views() {
+        return this.db.queryAll("advertiser_views", {
+            sort: [["timestamp", "DESC"]],
+        });
+    }
+    show_all_table_data() {
+        const advertiserData = this.db.queryAll("advertiser_data", {});
+        const advertiserViewsData = this.db.queryAll("advertiser_views", {});
+        console.log("advertiser_data");
+        console.log(advertiserData);
+        console.log("advertiser_views");
+        console.log(advertiserViewsData);
+    }
     add_new_advertiser_view(advertiser: Advertiser) {
         const uuid = uuidv4();
         const advertiser_uuid = sha1(advertiser.name);
+        const advertiser_name = advertiser.name;
         const timestamp = advertiser.captureTime;
         const platform = advertiser.captureSource;
         this.db.insert("advertiser_views", {
             uuid,
             advertiser_uuid,
+            advertiser_name,
             timestamp,
             platform,
         });
         this.db.commit();
+        // this.show_all_table_data();
     }
     add_new_advertiser(advertiser: Advertiser) {
         const uuid = sha1(advertiser.name);
@@ -49,7 +75,32 @@ export default class Database {
         const last_visited = advertiser.captureTime;
         const url = advertiser.link;
 
-        this.db.insertOrUpdate("advertiser_data", { uuid }, { ID: uuid });
+        const response = this.db.queryAll("advertiser_data", {
+            query: { uuid },
+        });
+        if (response.length > 0) {
+            // perform an update
+            this.db.update("advertiser_data", { uuid }, entry => {
+                entry.name = name;
+                entry.platform = platform;
+                entry.view_count = entry.view_count + 1;
+                entry.last_visited = last_visited;
+                entry.url = url;
+                return entry;
+            });
+        } else {
+            // perform an insert
+            this.db.insert("advertiser_data", {
+                uuid,
+                name,
+                platform,
+                view_count: 1,
+                last_visited,
+                url,
+            });
+        }
+
         this.db.commit();
+        // this.show_all_table_data();
     }
 }
