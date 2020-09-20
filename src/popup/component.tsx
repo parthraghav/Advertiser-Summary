7224a372326db5685cc2342aa349e2e57b8f01be
Parth Raghav <iniento12@gmail.com>
2020-09-20 12:49:41 +0530

    :construction: Working on Popup UI (1/2)

--- src/popup/component.tsx
+++ src/popup/component.tsx
@@ -1,8 +1,99 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import Messenger from "@src/messenger";
 import "./styles.scss";
+import { CaptureSource } from "@src/parsers/data";
+const axios = require("axios");
 
-// // // //
+interface AdvertiserHistoryGridProps {
+    view_history: number[];
+}
+
+const AdvertiserHistoryGrid = ({
+    view_history,
+}: AdvertiserHistoryGridProps) => {
+    return (
+        <div className="history-grid-wrapper">
+            {view_history.map((val: number, index: number) => (
+                <div
+                    key={index}
+                    className={"advertiser-history-brid-box fill-" + val}
+                ></div>
+            ))}
+        </div>
+    );
+};
+
+interface AdvertiserContainerProps {
+    index: number;
+    name: string;
+    view_count: number;
+    platform: CaptureSource;
+    url: string;
+    last_visited: number;
+    view_history: number[];
+}
+const AdvertiserContainer = ({
+    index,
+    name,
+    view_count,
+    platform,
+    url,
+    last_visited,
+    view_history,
+}: AdvertiserContainerProps) => {
+    const [logoUrl, setLogoUrl] = useState<any>(undefined);
+    // fetch the image url
+    useEffect(() => {
+        (async function() {
+            const urlParams = new URL(url);
+            const logoUrlApi =
+                "http://favicongrabber.com/api/grab/" + urlParams.host;
+            let cachedLogoUrl = window.localStorage.getItem(logoUrlApi);
+            if (cachedLogoUrl != null) {
+                setLogoUrl(cachedLogoUrl);
+                return;
+            }
+            const response = await axios.get(logoUrlApi);
+            let responseUrl = response.data.icons[0].src;
+            window.localStorage.setItem(logoUrlApi, responseUrl);
+            setLogoUrl(responseUrl);
+        })();
+    }, []);
+    return (
+        <div
+            className="hrow advertiser-row"
+            onClick={() => chrome.tabs.create({ url })}
+        >
+            <div className="index-container">
+                <span className="index-label">{index + 1}.</span>
+            </div>
+            <div className="logo-container">
+                <div className="logo-wrapper">
+                    {logoUrl && (
+                        <img className="advertiser-logo" src={logoUrl} />
+                    )}
+                </div>
+            </div>
+
+            <div className="info-container">
+                <span className="info-container-name-label">{name}</span>
+                <span className="info-container-view-label">
+                    Viewed {view_count} times
+                </span>
+                <div className="info-container-share-label">
+                    <span>Shared by</span>
+                    <img
+                        className="social-logo"
+                        src="https://facebookbrand.com/wp-content/uploads/2019/10/flogo_RGB_HEX-BRC-Site-250.png?w=250&h=250"
+                    />
+                </div>
+            </div>
+            <div className="history-grid-container">
+                <AdvertiserHistoryGrid view_history={view_history} />
+            </div>
+        </div>
+    );
+};
 
 export const Popup = () => {
     const messenger = new Messenger("Popup");
@@ -36,16 +127,27 @@ export const Popup = () => {
     console.log(advertiserData, advertiserViews);
     // Renders the component tree
     return (
-        <div className="">
-            <h1>Advertiser Data</h1>
-            {advertiserData.map((entry: any, index: number) => (
-                <p key={index}>{entry.name}</p>
-            ))}
-            <hr />
-            <h1>Advertiser Views</h1>
-            {advertiserViews.map((entry: any, index: number) => (
-                <p key={index}>{entry.advertiser_name}</p>
-            ))}
+        <div className="popup-container">
+            <div className="page-container">
+                <div className="hrow title-container">
+                    <h4>Top Advertisers</h4>
+                    <h6>targeting you right now</h6>
+                </div>
+                <div className="hrow data-container">
+                    {advertiserData.map((entry: any, index: number) => (
+                        <AdvertiserContainer
+                            key={index}
+                            index={index}
+                            name={entry.name}
+                            view_count={entry.view_count}
+                            platform={entry.platform}
+                            url={entry.url}
+                            last_visited={entry.last_visited}
+                            view_history={[0, 1, 2, 5, 3, 1, 4]}
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
