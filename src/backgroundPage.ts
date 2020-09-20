fd68b8498725ffabe50af1a0be6381e9186cae01
Parth Raghav <iniento12@gmail.com>
2020-09-20 10:25:49 +0530

    :bug: Bug fixes

--- src/backgroundPage.ts
+++ src/backgroundPage.ts
@@ -1,11 +1,37 @@
 import { browser } from "webextension-polyfill-ts";
 import * as config from "@src/config";
+import Database from "@src/database";
+import { Advertiser } from "@src/parsers/data";
+import Messenger from "@src/messenger";
+
+const db = new Database();
 
 // Listen for messages sent from other parts of the extension
 browser.runtime.onConnect.addListener(port => {
+    console.log(port.name);
     console.assert(port.name == config.PORT_NAME);
     port.onMessage.addListener(function(msg) {
-        if (msg.subject == "NEW_AD_DATA" && msg.from == "Content") {
+        const messenger = new Messenger("Background", port);
+        if (msg.subject == "PUT_AD_DATA" && msg.from == "Content") {
+            console.log("data write request received");
+            const advertiser: Advertiser = msg.data_packet;
+            db.add_new_advertiser(advertiser);
+            db.add_new_advertiser_view(advertiser);
+        }
+        if (msg.subject == "GET_AD_DATA" && msg.from == "Popup") {
+            console.log("data read request received");
+            const advertiser_data = db.get_all_advertiser_data();
+            const advertiser_views = db.get_all_advertiser_views();
+            console.log(advertiser_data);
+            console.log(advertiser_views);
+            messenger.sendRetrievedDataResponse(
+                advertiser_data,
+                "advertiser_data",
+            );
+            messenger.sendRetrievedDataResponse(
+                advertiser_views,
+                "advertiser_views",
+            );
         }
         // if (msg.joke == "Knock knock")
         //     port.postMessage({ question: "Who's there?" });
